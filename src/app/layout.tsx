83e9a9fb6761331db0a2cdd49d4282380d610c71
chouaib-djaidri <djaidri.chouaib.24@gmail.com>
2024-05-13 23:16:52 +0100

    set up project structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Galaxy Meet",
+  description:
+    "Galaxy Meet is a feature-rich web application for seamless online meetings, offering robust controls and functionalities to host and join meetings, manage settings, and access past and upcoming meetings with ease.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("font-poppins", poppins.variable)}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h2 className="text-center p-12 text-4xl font-bold">Galaxy Meet</h2>;
+}
